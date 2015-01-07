b69d96bc61d72712db15b2585f1e1106c2ce6a31
btkomodan <komodan@pm.nttdata.co.jp>
2015-01-07 20:11:30 +0900

    #1 delete unneccessary files

--- terasoluna-batch-tutorial/scripts/B000001.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-CD /D %~DP0
-CALL CLASSPATH.bat
-
-java -cp .:C:\Users\btkomodan.RDH\.m2\repository\* jp.terasoluna.fw.batch.executor.SyncBatchExecutor B000001
-
-ECHO %ERRORLEVEL%
-
-pause
