7afba836025477158b80f2f4f7d427ee6061c006
btkomodan <komodan@pm.nttdata.co.jp>
2015-01-07 19:55:04 +0900

    #1 fix B000001.bat

--- terasoluna-batch-blank/scripts/B000001.bat
+++ terasoluna-batch-blank/scripts/B000001.bat
@@ -2,7 +2,7 @@
 CD /D %~DP0
 CALL CLASSPATH.bat
 
-java -cp .:C:\Users\btkomodan.RDH\.m2\repository\* jp.terasoluna.fw.batch.executor.SyncBatchExecutor B000001
+java jp.terasoluna.fw.batch.executor.SyncBatchExecutor B000001
 
 ECHO %ERRORLEVEL%
 
