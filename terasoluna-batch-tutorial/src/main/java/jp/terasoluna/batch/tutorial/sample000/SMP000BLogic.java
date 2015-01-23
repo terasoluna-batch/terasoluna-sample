a6131d014d58a10ff427fc15052d50d0247f4a4e
btkomodan <komodan@pm.nttdata.co.jp>
2015-01-23 15:23:35 +0900

    fix logger(commons-logging->slf4j) #1

--- terasoluna-batch-tutorial/src/main/java/jp/terasoluna/batch/tutorial/sample000/SMP000BLogic.java
+++ terasoluna-batch-tutorial/src/main/java/jp/terasoluna/batch/tutorial/sample000/SMP000BLogic.java
@@ -26,8 +26,8 @@ import jp.terasoluna.batch.tutorial.common.NyusyukkinData;
 import jp.terasoluna.fw.batch.blogic.AbstractTransactionBLogic;
 import jp.terasoluna.fw.batch.blogic.vo.BLogicParam;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
@@ -39,7 +39,7 @@ public class SMP000BLogic extends AbstractTransactionBLogic {
     /**
      * ログクラス。
      */
-    private static final Log log = LogFactory.getLog(SMP000BLogic.class);
+    private static final Logger log = LoggerFactory.getLogger(SMP000BLogic.class);
 
     @Inject
     protected SMP000Dao dao;
