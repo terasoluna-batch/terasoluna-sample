0cfa101cbbecd3dbf3c7945a10deb187642dc5c7
btkomodan <komodan@pm.nttdata.co.jp>
2015-02-03 10:43:21 +0900

    changed path PostgreSQL8.4 to PostgreSQL9.3 #2

--- terasoluna-batch-functionsample/sql/postgres/init_job_control.bat
+++ terasoluna-batch-functionsample/sql/postgres/init_job_control.bat
@@ -1,4 +1,4 @@
-set path=C:\Program Files\PostgreSQL\8.4\bin\;%path%
+set path=C:\Program Files\PostgreSQL\9.3\bin\;%path%
 psql -h 127.0.0.1 -U postgres -d functionsampledb -f insert_job_control.sql
 
 pause
