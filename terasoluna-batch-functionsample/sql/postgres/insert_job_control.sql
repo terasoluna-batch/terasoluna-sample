7d7bdf522d3be73eacf7315639669f0f221d7fe7
btkomodan <komodan@pm.nttdata.co.jp>
2015-02-12 14:56:09 +0900

    added client encoding settings and removed unneccessary instructions #52

--- terasoluna-batch-functionsample/sql/postgres/insert_job_control.sql
+++ terasoluna-batch-functionsample/sql/postgres/insert_job_control.sql
@@ -1,5 +1,3 @@
 TRUNCATE TABLE job_control;
 
 INSERT INTO job_control (job_seq_id, job_app_cd, job_arg_nm1, job_arg_nm2, job_arg_nm3, job_arg_nm4, job_arg_nm5, job_arg_nm6, job_arg_nm7, job_arg_nm8, job_arg_nm9, job_arg_nm10, job_arg_nm11, job_arg_nm12, job_arg_nm13, job_arg_nm14, job_arg_nm15, job_arg_nm16, job_arg_nm17, job_arg_nm18, job_arg_nm19, job_arg_nm20, blogic_app_status, cur_app_status, add_date_time, upd_date_time) VALUES ('0000000001', 'B002001', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', NULL, NULL);
-
-commit;
